f857c4a65b6b15793dd16deed0f76cf4df8938b2
adushinova_ekaterina <adushinova2002@mail.ru>
2021-12-27 16:57:15 +0800

    Создала список рандомных частиц

--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,25 @@ namespace Курсовая_работа
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>(); // список для хранения частиц
         public Form1()
         {
             InitializeComponent();
 
             // привяжем изображение, для того чтобы рисовать на нем
-            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height); 
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            
+            // генерируем 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                Particle particle = new Particle();
+
+                // переносим частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                
+                particles.Add(particle); // добавляем список
+            }
         }
     }
 }
